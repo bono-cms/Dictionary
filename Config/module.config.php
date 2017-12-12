3e347d58692c3a89b0e164d279d8cc103df6f65a
David Yang <daworld.ny@gmail.com>
2017-12-13 00:31:19 +0500

    Initial module configuration

---
+++ Config/module.config.php
@@ -0,0 +1,9 @@
+<?php
+
+return array(
+    'route' => 'Dictionary:Admin:Dictionary@indexAction',
+    'icon' => 'dictionary.png',
+    'name' => 'Dictionary',
+    'caption' => 'Dictionary',
+    'description' => 'Manage any labels and captions on your site'
+);
