802d5e8b70d922e109f5bc04b36ccde1631a66fb
dave-yang <daworld.ny@gmail.com>
2019-08-24 17:16:41 +0500

    Fixes in schema

--- Storage/MySQL/schema.sql
+++ Storage/MySQL/schema.sql
@@ -1,16 +1,16 @@
+
 DROP TABLE IF EXISTS `bono_module_cms_dictionary`;
 CREATE TABLE `bono_module_cms_dictionary` (
-
- `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
- `alias` varchar(255) COMMENT 'ID alias'
-
+    `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `alias` varchar(255) COMMENT 'ID alias'
 ) DEFAULT CHARSET = UTF8;
 
 DROP TABLE IF EXISTS `bono_module_cms_dictionary_translations`;
 CREATE TABLE `bono_module_cms_dictionary_translations` (
+    `id` INT NOT NULL,
+    `lang_id` INT NOT NULL COMMENT 'Language identificator of this page',
+    `value` TEXT,
 
- `id` INT NOT NULL,
- `lang_id` INT NOT NULL COMMENT 'Language identificator of this page',
- `value` TEXT
-
-) DEFAULT CHARSET = UTF8;
+    FOREIGN KEY (id) REFERENCES bono_module_cms_dictionary(id) ON DELETE CASCADE,
+    FOREIGN KEY (lang_id) REFERENCES bono_module_cms_languages(id) ON DELETE CASCADE
+) ENGINE = InnoDB DEFAULT CHARSET = UTF8;
