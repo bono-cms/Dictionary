fab8a08a557ddd2030afe04992c07974b1a849b6
dave-yang <daworld.ny@gmail.com>
2026-04-04 15:44:38 +0500

    Updated encoding

--- Storage/MySQL/schema.sql
+++ Storage/MySQL/schema.sql
@@ -4,7 +4,7 @@ CREATE TABLE `bono_module_cms_dictionary` (
     `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     `alias` varchar(255) COMMENT 'ID alias',
     `wysiwyg` BOOLEAN NOT NULL COMMENT 'Whether enable wysiwyg'
-) DEFAULT CHARSET = UTF8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4_unicode_ci;
 
 DROP TABLE IF EXISTS `bono_module_cms_dictionary_translations`;
 CREATE TABLE `bono_module_cms_dictionary_translations` (
@@ -14,4 +14,4 @@ CREATE TABLE `bono_module_cms_dictionary_translations` (
 
     FOREIGN KEY (id) REFERENCES bono_module_cms_dictionary(id) ON DELETE CASCADE,
     FOREIGN KEY (lang_id) REFERENCES bono_module_cms_languages(id) ON DELETE CASCADE
-) ENGINE = InnoDB DEFAULT CHARSET = UTF8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4_unicode_ci;
